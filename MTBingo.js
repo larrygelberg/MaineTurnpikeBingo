ab7a7c2fa9c5f2e47176ffaf4293a41fd18b7e7f
larrygelberg <bebopdaddy@gmail.com>
2023-03-12 17:08:10 -0400

    Fixed problem with refreshing statusTable

--- MTBingo.js
+++ MTBingo.js
@@ -71,7 +71,8 @@ function getMessage() {
 
 function refreshGrid() {
   usedIndexes = [];
-
+  statusTable = [];
+  
   for (var i=0; i<5; i++) { 
     for (var j=0; j<5; j++) {
       if (i == 2 && j == 2) {
